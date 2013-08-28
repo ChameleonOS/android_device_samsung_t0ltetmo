ddde8c15fcfea9109788617e4fa4ae3be601f8c9
0xD34D <clark@scheffsblend.com>
2013-08-28 11:41:28 -0700

    Update for ChameleonOS

--- cos.mk
+++ cos.mk
@@ -1,5 +1,5 @@
 # Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cos/config/gsm.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := t0ltetmo
@@ -8,15 +8,15 @@ PRODUCT_RELEASE_NAME := t0ltetmo
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common ChaOS stuff.
+$(call inherit-product, vendor/cos/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/t0ltetmo/full_t0ltetmo.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := t0ltetmo
-PRODUCT_NAME := cm_t0ltetmo
+PRODUCT_NAME := cos_t0ltetmo
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SGH-T889
 PRODUCT_MANUFACTURER := samsung
